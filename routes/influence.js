355a9b13a7d00c0b195fb5ae13e6bd7c0c3b561a
Rohit Kumar Verma <rohitkverma77@gmail.com>
2020-03-11 15:20:08 +0530

    added basic routing

---
+++ routes/influence.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require("path");
+const router = express.Router();
+
+
+// router.get('/', (req, res)=>{
+//     res.sendFile(path.dirname(__dirname)+"/html/homePage.html");
+// });
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mysql = require("mysql");
+const bcrypt = require("bcrypt");
+
+const app = express();
+// const PORT=3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// const a=require(__dirname+"/routes/influence.js");
+
+app.use("/", require(__dirname+"/routes/home.js"));
+app.use("/signup", require(__dirname+"/routes/signUp.js"));
+app.use("/signin", require(__dirname+"/routes/signIn.js"));
+app.use("/mycreation", require(__dirname+"/routes/myCreation.js"));
+app.use("/influence", require(__dirname+"/routes/influence.js"));
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server Started at PORT "+process.env.PORT);
+});
