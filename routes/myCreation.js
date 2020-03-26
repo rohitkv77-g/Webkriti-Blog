5aa1c8c5f86053c9b4a8447a239618002a7a4f25
Abhishek Singh Chauhan <abhisheksinghchauhan357@gmail.com>
2020-03-27 02:43:28 +0530

    in myCreation user can see all his blogs but cannot delete or readmore it

--- routes/myCreation.js
+++ routes/myCreation.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const path = require("path");
 const router = express.Router();
+const mySqlConnection = require("../database/db");
 
 
 // router.get('/', (req, res)=>{
 //     res.sendFile(path.dirname(__dirname)+"/html/homePage.html");
 // });
-router.get("/",(req,res)=>{
-    res.render("myCreation.ejs",{});
+// router.get("/",(req,res)=>{
+//     res.render("myCreation.ejs",{});
+// });
+
+router.get("/", function(req, res){
+    if (req.session.user) {
+        mySqlConnection.query(
+            "SELECT * from blogs where authorName = ?", [req.session.user],
+            (err, rows) => {
+                if (err) res.status(500).send(err)
+                // else if(rows.length == 0) res.send("You have no Blogs to display")
+                else{
+                    res.status = 200
+                    res.render('myCreation', {blogs : rows})
+                }  
+            },
+            )
+    }else{
+        res.redirect("/signin?login+first");
+    }      
 });
 
 
