d65faeb309853fab8c310763e4c5a82079724c8b
Abhishek Singh Chauhan <abhisheksinghchauhan357@gmail.com>
2020-03-28 01:03:13 +0530

    database made online just change your .env file

--- database/db.js
+++ database/db.js
@@ -1,15 +1,31 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
+
+// const mySqlConnection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: process.env.DATABASE_PASSWORD,
+//   database: "blogDatabase",
+// });
+
+// mySqlConnection.connect((err) => {
+//   if (err) res.status(500).send(err);
+//   console.log("Database Connected!");
+// });
+
+// module.exports = mySqlConnection;
+
+const mysql = require("mysql");
 
 const mySqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: process.env.DATABASE_PASSWORD,
-  database: "blogDatabase",
+host: "sql12.freemysqlhosting.net",
+user: "sql12329703",
+password: process.env.DATABASE_PASSWORD_ONLINE,
+database: "sql12329703"
 });
 
-mySqlConnection.connect((err) => {
-  if (err) res.status(500).send(err);
-  console.log("Database Connected!");
+mySqlConnection.connect(err => {
+if (err) console.log(err);
+else console.log("Database Connected!");
 });
 
 module.exports = mySqlConnection;
